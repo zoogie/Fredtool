f9697a17abe6a9b117803852844c3b9a9bf8047f
zoogie <secretAgentCokey@gmail.com>
2019-01-10 03:29:22 -0600

    refactoring & cleanup

--- source/main.cpp
+++ source/main.cpp
@@ -4,8 +4,7 @@
 #include "crypto.h"
 #include "tadpole.h"
 #include "types.h"
-#include "hash_stash.h"
-//0x4000+0x20+0xF0+0x20+0x4E0+0x20+0xB40+0x20+0x00218800+0x20 = 0x21D9B0  
+#include "hash_stash.h" 
 
 void error(const char *errormsg, const char *filename, bool fatal) {
 	printf("%s:%s %s\nHit Enter to close\n", fatal ? "ERROR":"WARNING", errormsg, filename);
@@ -13,8 +12,9 @@ void error(const char *errormsg, const char *filename, bool fatal) {
 	if(fatal) exit(-1); 
 }
 
-u8 *readAllBytes(const char *filename, u32 &filelen) {
+u8 *readAllBytes(const char *filename, u32 &filelen, u32 custom_memsize) {
 	FILE *fileptr = fopen(filename, "rb");
+	u32 buflen=0;
 	if (fileptr == NULL) {
 		error("Failed to open ", filename, true);
 	}
@@ -24,11 +24,19 @@ u8 *readAllBytes(const char *filename, u32 &filelen) {
 	rewind(fileptr);
 	
 	if(filelen > 0x21E000) filelen=0x21E000; //keep dsiware buffer reasonable - some dsiwares can get really large and we don't need to go over flipnote injected DS dlp size
+	
+	buflen=filelen; 
+	if(custom_memsize){
+		buflen=custom_memsize;
+		if(buflen<filelen) buflen=filelen;  //sanity
+	}
 
-	u8 *buffer = (u8*)malloc(filelen);
+	u8 *buffer = (u8*)malloc(buflen);
 
 	fread(buffer, filelen, 1, fileptr);
 	fclose(fileptr);
+	
+	//printf("%s %08X %08X %08X\n", filename, filelen, buflen, custom_memsize);
 
 	return buffer;
 }
@@ -54,41 +62,46 @@ void dumpMsedData(u8 *msed){
 }
 
 void makeTad(char *filename, u32 ishax) {
+	// === DATA ===
 	u8 *dsiware, *resources, *ctcert, *injection, *tmd, *banner, *movable;
 	u32 dsiware_size, resources_size, ctcert_size, movable_size, injection_size, banner_size, tmd_size;
 	u8 header_hash[0x20] = {0}, srl_hash[0x20] = {0}, tmp_hash[0x20] = {0}, tmd_hash[0x20]={0}, banner_hash[0x20]={0};
 	u8 normalKey[0x10] = {0}, normalKey_CMAC[0x10] = {0};
-	//char outfile[64]={0};
+	u8 header[0xF0]={0};
 	header_t header_out;
 	memset(&header_out, 0, 0xF0);
 
+	// === PREP ===
 	printf("\nReading %s\n", filename);
-	dsiware = readAllBytes(filename, dsiware_size);
+	dsiware = readAllBytes(filename, dsiware_size, 0x21E000);
 	if (dsiware_size > 0x4000000) {
 		error("Provided dsiware seems to be way too large!","", true);
 	}
 	
 	printf("Reading & parsing movable.sed\n");
-	movable = readAllBytes("movable.sed", movable_size);
+	movable = readAllBytes("movable.sed", movable_size, 0);
 	if (movable_size != 320 && movable_size != 288) {
 		error("Provided movable.sed is not 320 or 288 bytes of size","", true);
 	}
 	
 	printf("Reading resources/frogcertXL.bin\n");
-	resources = readAllBytes(ishax ? "resources/frogcertXL.bin" : "resources/dlpcertXL.bin", resources_size);
+	resources = readAllBytes(ishax ? "resources/frogcertXL.bin" : "resources/dlpcertXL.bin", resources_size, 0);
 	calculateSha256(resources, resources_size, tmp_hash);
 	if (memcmp(tmp_hash, ishax ? frogcertXL_hash : dlpcertXL_hash, 0x20) != 0) {
 		error("Provided resources file's hash doesn't match","", true);
 	}
 	
 	injection = resources;
-	injection_size = resources_size - 0x4CDE;
+	injection_size = resources_size - 0x4CDE; 
 	banner = injection + injection_size;
 	banner_size = 0x4000;
 	tmd = banner + banner_size;
 	tmd_size = 0xB40;
 	ctcert = tmd + tmd_size;
 	ctcert_size = 0x19E;
+	
+	dsiware_size = injection_size + 0x51B0;                      //or (0x4000+0x20)+(0xF0+0x20)+(0x4E0+0x20)+(0xB40+0x20)+(injection_size+0x20) = 0x21D9B0 or 0x6ED70
+	printf("New DSiWare size: %08X - Expected: %08X\n", dsiware_size, (0x4000+0x20)+(0xF0+0x20)+(0x4E0+0x20)+(0xB40+0x20)+(injection_size+0x20));  //injection size 0x00218800 or 0x00069BC0
 	
 	if(ishax){ //only need to do this once
 		printf("Dumping msed_data.bin\n");
@@ -99,15 +112,19 @@ void makeTad(char *filename, u32 ishax) {
 	keyScrambler((movable + 0x110), false, normalKey);
 	keyScrambler((movable + 0x110), true, normalKey_CMAC);
 	
-	// === HEADER ===
+	// === KEYY TEST ===
 	printf("Decrypting header\n");
-	u8 *header = new u8[0xF0];
 	getSection((dsiware + 0x4020), 0xF0, normalKey, header);
 	
 	if (memcmp("3FDT", header, 4)) {
 		error("Decryption failed","", true);
 	}
 	
+	// === BANNER ===
+	printf("Placing back banner\n");
+	placeSection((dsiware + 0x0), banner, banner_size, normalKey, normalKey_CMAC);
+	
+	// === HEADER ===
 	printf("Writing new header data\n");
 	header_out.magic=0x54444633; //"3FDT"
 	header_out.group_id=0;
@@ -122,42 +139,20 @@ void makeTad(char *filename, u32 ishax) {
 
 	printf("Placing back header\n");
 	placeSection((dsiware + 0x4020), (u8*)&header_out, 0xF0, normalKey, normalKey_CMAC);
-
-	printf("Calculating new header hash\n");
-	calculateSha256((u8*)&header_out, 0xF0, header_hash);
-	delete[] header;
-
-	// === SRL.NDS ===
-	// Basically, the srl.nds of DS Download play is right at the end of the TAD container
-	// Because we don't care about what it contains, we can overwrite it directly with our new
-	// flipnote srl.nds straight off the bat, without having to do any decryption
-	// We of course need to extend our dsiware array by the necessary difference in bytes
-	// to accomodate the new flipnote srl.nds (which is 0x218800 in size)
-	printf("Resizing array\n");
-	printf("Old DSiWare size: %08X\n", dsiware_size);
-	dsiware_size = injection_size + 0x51B0;
-	printf("New DSiWare size: %08X\n", dsiware_size);
-	dsiware = (u8*)realloc(dsiware, dsiware_size);
-	printf("Placing back srl.nds\n");
-	placeSection((dsiware + 0x5190), injection, injection_size, normalKey, normalKey_CMAC);
-
-	printf("Calculating new srl, banner, and tmd hashes\n");
-	calculateSha256(injection, injection_size, srl_hash);
-	calculateSha256(banner, banner_size, banner_hash);
-	calculateSha256(tmd, tmd_size, tmd_hash);
 	
-	// === BANNER ===
-	placeSection((dsiware + 0x0), banner, banner_size, normalKey, normalKey_CMAC);
-	
-	// === TMD ===
-	placeSection((dsiware + 0x4630), tmd, tmd_size, normalKey, normalKey_CMAC);
-
 	// === FOOTER ===
-	printf("Decrypting footer\n");
+	printf("Calculating new banner, header, tmd, and srl hashes\n");
+	calculateSha256(banner, banner_size, banner_hash);
+	calculateSha256((u8*)&header_out, 0xF0, header_hash);
+	calculateSha256(tmd, tmd_size, tmd_hash);
+	calculateSha256(injection, injection_size, srl_hash);
+	
+	printf("Generating footer\n");
 	footer_t *footer = (footer_t*)malloc(SIZE_FOOTER);
-	getSection((dsiware + 0x4130), 0x4E0, normalKey, (u8*)footer);
+	memset(footer, 0,0x4E0);
+	memcpy((u8*)(&footer->banner_hash[0]+0x2A0), "AP0000000000000000", 0x12);
 
-	printf("Fixing hashes\n");
+	printf("Fixing footer hashes\n");
 	memcpy(footer->banner_hash , banner_hash, 0x20); //Fix the banner hash
 	memcpy(footer->hdr_hash , header_hash, 0x20); //Fix the header hash
 	memcpy(footer->tmd_hash , tmd_hash, 0x20); //Fix the tmd hash
@@ -173,6 +168,15 @@ void makeTad(char *filename, u32 ishax) {
 	printf("Placing back footer\n");
 	placeSection((dsiware + 0x4130), (u8*)footer, 0x4E0, normalKey, normalKey_CMAC);
 	
+	// === TMD ===
+	printf("Placing back TMD\n");
+	placeSection((dsiware + 0x4630), tmd, tmd_size, normalKey, normalKey_CMAC);
+
+	// === SRL.NDS ===
+	printf("Placing back srl.nds\n");
+	placeSection((dsiware + 0x5190), injection, injection_size, normalKey, normalKey_CMAC);
+	
+	// === OUTPUT ===
 	const char *outname = ishax ? "output/hax/484E4441.bin" : "output/clean/484E4441.bin";
 
 	printf("Writing %s\n", outname);
@@ -188,7 +192,7 @@ void makeTad(char *filename, u32 ishax) {
 }
 
 int main(int argc, char* argv[]) {
-
+	printf("Fredtool v1.0 - zoogie , jason0597\n");
 	makeTad(argv[1], 1); //DS dlp injected with JPN flipnote v0
 	makeTad(argv[1], 0); //DS dlp clean, intended for restoring title to prehax state
 	printf("\nJobs completed\n");
